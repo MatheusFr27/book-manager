8baf60c0b134168019d47ccee99287c0ea283a1d
MatheusFr27 <matheusefranca1727@gmail.com>
2021-02-02 19:52:52 -0300

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'book-manager'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
