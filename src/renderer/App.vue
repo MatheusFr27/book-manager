c6704e061e0d958d7a7a583886d122d91dfda715
MatheusFr27 <matheusefranca1727@gmail.com>
2021-02-03 20:44:28 -0300

    Modificações no css do app

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,11 +5,24 @@
 </template>
 
 <script>
-  export default {
-    name: 'book-manager'
-  }
+export default {
+  name: "book-manager",
+};
 </script>
 
 <style>
-  /* CSS */
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap");
+
+html,
+body {
+  font-size: 12pt;
+  font-family: "Oswald", sans-serif;
+  background-color: #fdfdfd;
+
+  padding: 0;
+  margin: 0;
+
+  height: 100%;
+  width: 100%;
+}
 </style>
