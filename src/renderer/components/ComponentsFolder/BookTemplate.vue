5b80ba924744ad9bf3a3d76103439bc5a8890303
MatheusFr27 <matheusefranca1727@gmail.com>
2021-02-06 21:53:54 -0300

    Modelo de visualização de livro criado

---
+++ src/renderer/components/ComponentsFolder/BookTemplate.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="book in bookStructure" :key="book.id">
+      <h1>{{ book.title }}</h1>
+      <p>{{ book.genre }} || {{ book.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookTemplate",
+  props: {
+    bookStructure: {
+      type: Array,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style></style>
