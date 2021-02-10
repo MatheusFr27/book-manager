68333877d00f1435881582f5de69ac6f88b377a6
MatheusFr27 <matheusefranca1727@gmail.com>
2021-02-10 18:37:33 -0300

    Adicionado previamente as páginas de adm e configuração

---
+++ src/renderer/components/ConfigurationPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <ul>
+      <li>Aqui eu terei opção de Tema Padrão, branco ou escuro</li>
+      <li>Aumentar ou diminuir letra</li>
+      <li>Transforma toda font em arial</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ConfigurationPage",
+};
+</script>
+
+<style></style>
